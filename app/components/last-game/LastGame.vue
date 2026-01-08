ace02464a541a4d1cc2626c37a263ddd9ff4c330
Sergey Kovalchuk <81366846+SergeyKovalchuk2086@users.noreply.github.com>
2026-01-08 16:40:41 +0700

    add last game card

---
+++ app/components/last-game/LastGame.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="last-game">
+    <div class="last-game__header">
+      <span>{{ homeTeam }}</span>
+      <span>{{ awayTeam }}</span>
+    </div>
+
+    <div class="last-game__score">
+      <span>{{ homeScore }}</span>
+      <span>{{ awayScore }}</span>
+    </div>
+
+    <div class="last-game__date">
+      {{ date }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  homeTeam: string;
+  awayTeam: string;
+  homeScore: number;
+  awayScore: number;
+  date: string;
+}>();
+</script>
+
+<style scoped lang="css">
+.last-game {
+  padding: 12px 24px;
+  background-color: #ffb81d;
+  border-radius: 8px;
+}
+
+.last-game__header,
+.last-game__score {
+  display: flex;
+}
+
+.last-game__header span,
+.last-game__score span {
+  width: 50%;
+  text-align: center;
+}
+
+.last-game__header {
+  margin-bottom: 8px;
+  font-size: 14px;
+}
+
+.last-game__score {
+  font-size: 30px;
+  font-weight: 600;
+  letter-spacing: 1px;
+}
+
+.last-game__date {
+  margin-top: 10px;
+  text-align: center;
+  font-size: 12px;
+  opacity: 0.8;
+}
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,13 @@
 <template>
   <TopSection class="top-setion" />
   <TopPlayers class="top-players" />
+  <LastGame
+    home-team="Чёрные"
+    away-team="Белые"
+    :home-score="15"
+    :away-score="20"
+    date="7.01.2026 21:00–21:15"
+  />
 </template>
 
 <script setup lang="ts">
